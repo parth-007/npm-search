8f58d3477dc3cacc6bfd3c444aa337637a31c11f
parth-007 <parthmangukiya77@gmail.com>
2021-08-24 21:31:04 +0530

    Added Actions

--- src/components/RepoList.tsx
+++ src/components/RepoList.tsx
@@ -1,15 +1,18 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
+import { useActions } from '../hooks/useAction';
 import { actionCreators } from '../state';
 
 const RepoList: React.FC = () => {
     const [term, setTerm] = useState('');
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
+    const { searchRepos } = useActions();
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
-        dispatch(actionCreators.searchRepos(term)); 
+        // dispatch(actionCreators.searchRepos(term)); 
+        searchRepos(term);
     }
     return (
         <div>
