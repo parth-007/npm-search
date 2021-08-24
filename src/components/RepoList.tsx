2949c53842b43b14b8307d84b65c5fa42481ca52
parth-007 <parthmangukiya77@gmail.com>
2021-08-24 21:41:50 +0530

    Starting with weird typings with react-redux

--- src/components/RepoList.tsx
+++ src/components/RepoList.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 // import { useDispatch } from 'react-redux';
+// import { actionCreators } from '../state';
 import { useActions } from '../hooks/useAction';
-import { actionCreators } from '../state';
+import { useSelector } from 'react-redux';
+// useSelector same as mapPropsToState (Used in class based component)
 
 const RepoList: React.FC = () => {
     const [term, setTerm] = useState('');
     // const dispatch = useDispatch();
     const { searchRepos } = useActions();
+
+    const {data, erorr, loading} = useSelector((state) => state.repos);
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
